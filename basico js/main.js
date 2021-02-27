0ca75bdc66926ac316066b52e47626f648aef545
santiago <santiagosacc2006@gmail.com>
2021-02-26 19:06:52 -0500

    BasicData

---
+++ basico js/main.js
@@ -0,0 +1,18 @@
+//let es una variable de bloque y var es una variable global
+let numero = "numero uno";
+
+//const es una variable constante cuando este usando el dom lo vere mas a a fondo
+const nombre = "sara";
+ var edad = 23;
+
+//con console.log nos muestra en consola los valores
+ console.log(nombre,numero,edad);
+
+
+ //arrays 
+ const myArray =[ "sara","santiago","camila","papa" ];
+ console.log(myArray);
+
+ //crear un array
+const newArray = new Array(1,2,3,4,5,6);
+console.log(newArray)
