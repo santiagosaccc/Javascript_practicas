73edb4f2531167a96d4d9410be9709336bc158ed
Santiago Isaac Ramos Vargas <63522159+santiagosaccc@users.noreply.github.com>
2021-03-01 22:11:58 -0500

    new study

---
+++ basico js/let_const.js
@@ -0,0 +1,28 @@
+const numero = 'santago';
+let numeros= 24;
+console.log(numeros + " " + numero);
+//resordemos que el cosnt tiene un avariable que no puede ser cambiada 
+//let es una variable de bloque
+
+if (true){
+
+    let numeros = 60;// que sucede aca que la variable 
+    console.log(numeros)
+}
+console.log(numeros)
+
+///parametros para funcion de flecha y funcion normal 
+function myFuction(){
+   console.log(50)
+}
+myFuction()//por medio de esta declaracion 
+
+///funciones de flecha
+let newFuction = () =>{//por medio de esto podemos ver la diferencia de argumentos para ejecutar la funcion de flecha 
+
+    console.log(800)
+}
+newFuction()
+
+
+
