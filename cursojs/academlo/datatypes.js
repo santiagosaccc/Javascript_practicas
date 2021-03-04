4375ac3c409e91ef8471b0d5ac24efff3bc3c3d2
Santiago Isaac Ramos Vargas <63522159+santiagosaccc@users.noreply.github.com>
2021-03-03 20:39:42 -0500

    add config

---
+++ cursojs/academlo/datatypes.js
@@ -0,0 +1,12 @@
+//ponemos y comncatenamos variables
+const newname = 'erick';
+const namelet = 'vargas';
+
+const concat = newname +' '+ namelet;
+//console.log(concat);
+
+///concatenar en texto o en numeros
+
+const age = '34';//en este caso me esta dando es texto y lo tomara como texto
+//si se suman dos strings se concatenan y no se suman 
+const numero = 45;  ///de esta forma ya tenemos un numero
--- cursojs/py.py
+++
@@ -1,9 +0,0 @@
-import json 
-
-x = {
-    "name":"santiago"
-}
-
-y = json.dumps(x)
-print(y)
-'''con este comando logramos trasfrmar de json a python'''
