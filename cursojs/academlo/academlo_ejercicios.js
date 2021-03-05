b634eabedd1a1d96122c2292f14d19da29844b82
Santiago Isaac Ramos Vargas <63522159+santiagosaccc@users.noreply.github.com>
2021-03-04 21:56:34 -0500

    new function

--- cursojs/academlo/academlo_ejercicios.js
+++ cursojs/academlo/academlo_ejercicios.js
@@ -1,4 +1,35 @@
-let age = 3;
-console.log(age)
+function sumar (){
 
-//ejercisio simple
+    let a ,b ,c,d ;
+    a=3; b=5; c=7; d=9;
+    return a+b+c+d;
+    
+    }
+    sumar()
+    
+    
+    
+    
+    function restas (){
+        let b1,b2;
+        b1=3; b2=5;
+         return  b1-b2;
+    }
+    function reusar (){
+        const m3 = restas();
+        const m5 = sumar();
+        console.log(m5,m3)
+    }
+    reusar()
+    
+    ///funcion con parametros
+    
+    
+    function parametrs (a,c){
+        console.log(a+c)
+    }
+    parametrs(8,4)
+    //por medio de estos parametros hacemos cosas mas complejas
+    
+    
+    
--- cursojs/academlo/string-functions.js
+++ cursojs/academlo/string-functions.js
@@ -1,2 +1,12 @@
 //cuantas letras tiene el siguiente texto 
 const fullName = 'santiago ramos ';
+// 
+const primera = "Hoy es un buen dia para programar ";
+const newfuc = primera.slice(-10,-1);
+///
+
+///solucion de ejercisions
+let frase = "hoy es un buen dia para 'programar' ";
+let convert = frase[25];
+console.log(convert);
+
